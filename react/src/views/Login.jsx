10d954b7d8a7600e7b3fbac84b9911202c2e1c1c
yodanisesutantio <yodanise66@gmail.com>
2025-03-29 09:48:31 +0700

    Login and signup page is done, we can move on to the next phase

--- react/src/views/Login.jsx
+++ react/src/views/Login.jsx
@@ -7,16 +7,39 @@ const Login = () => {
   };
 
   return (
-    <div className="login-signup-form">
-      <div className="form">
-        <form action="" onSubmit={onSubmit}>
-          <h1 className="title">Login into your account</h1>
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="Password" />
-          <button className="btn btn-block">Login</button>
+    <div className="w-dvw h-dvh flex justify-center items-center">
+      <div className="w-[30%] bg-[#181818] py-6 px-12 shadow-[3px_3px_25px] shadow-emerald-950/40">
+        <form
+          action=""
+          onSubmit={onSubmit}
+          className="flex flex-col gap-4 items-center justify-center rounded-2xl overflow-hidden"
+        >
+          <h1 className="text-3xl text-center font-bold tracking-tighter mb-6">
+            Log into your account
+          </h1>
 
-          <p className="message">
-            Not Registered? <Link to="/signup">Create an Account!</Link>
+          <input
+            type="email"
+            placeholder="Email Address"
+            className="w-full border border-slate-300/30 bg-slate-300/5 text-slate-300 placeholder:text-slate-300/30 py-3 px-4 rounded-lg"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            className="w-full border border-slate-300/30 bg-slate-300/5 text-slate-300 placeholder:text-slate-300/30 py-3 px-4 rounded-lg"
+          />
+          <button className="w-full mt-3 py-2 bg-emerald-500 hover:bg-emerald-600 text-[#181818] font-bold tracking-tighter text-lg rounded-lg duration-300">
+            Login
+          </button>
+
+          <p className="text-slate-100">
+            Not Registered?{" "}
+            <Link
+              to="/signup"
+              className="text-emerald-500 underline hover:no-underline"
+            >
+              Create an Account!
+            </Link>
           </p>
         </form>
       </div>
--- react/src/views/Signup.jsx
+++ react/src/views/Signup.jsx
@@ -7,18 +7,48 @@ const Signup = () => {
   };
 
   return (
-    <div className="login-signup-form">
-      <div className="form">
-        <form action="" onSubmit={onSubmit}>
-          <h1 className="title">Register Account</h1>
-          <input type="email" placeholder="Full Name" />
-          <input type="email" placeholder="Email Address" />
-          <input type="password" placeholder="Password" />
-          <input type="password" placeholder="Password Confirmation" />
-          <button className="btn btn-block">Signup</button>
+    <div className="w-dvw h-dvh flex justify-center items-center">
+      <div className="w-[30%] bg-[#181818] py-6 px-12 shadow-[3px_3px_25px] shadow-emerald-950/40">
+        <form
+          action=""
+          onSubmit={onSubmit}
+          className="flex flex-col gap-4 items-center justify-center rounded-2xl overflow-hidden"
+        >
+          <h1 className="text-3xl text-center font-bold tracking-tighter mb-6">
+            Register Account
+          </h1>
+          <input
+            type="email"
+            placeholder="Full Name"
+            className="w-full border border-slate-300/30 bg-slate-300/5 text-slate-300 placeholder:text-slate-300/30 py-3 px-4 rounded-lg"
+          />
+          <input
+            type="email"
+            placeholder="Email Address"
+            className="w-full border border-slate-300/30 bg-slate-300/5 text-slate-300 placeholder:text-slate-300/30 py-3 px-4 rounded-lg"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            className="w-full border border-slate-300/30 bg-slate-300/5 text-slate-300 placeholder:text-slate-300/30 py-3 px-4 rounded-lg"
+          />
+          <input
+            type="password"
+            placeholder="Password Confirmation"
+            className="w-full border border-slate-300/30 bg-slate-300/5 text-slate-300 placeholder:text-slate-300/30 py-3 px-4 rounded-lg"
+          />
+          <button className="w-full mt-3 py-2 bg-emerald-500 hover:bg-emerald-600 text-[#181818] font-bold tracking-tighter text-lg rounded-lg duration-300">
+            Signup
+          </button>
 
-          <p className="message">
-            Already Registered? <Link to="/login">Login Instead!</Link>
+          <p className="text-slate-100">
+            Already Registered?{" "}
+            <Link
+              to="/login"
+              className="text-emerald-500 underline hover:no-underline"
+            >
+              Login Instead!
+            </Link>
           </p>
         </form>
       </div>
